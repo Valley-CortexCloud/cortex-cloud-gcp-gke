5c172d94511d8a6b57b6163f206364abaf63d465
jvalley19 <52843322+jvalley19@users.noreply.github.com>
2026-03-10 16:40:18 -0400

    Update main.tf

--- terraform/gcp/network-hub/main.tf
+++ terraform/gcp/network-hub/main.tf
@@ -1,116 +1,72 @@
 # ------------------------------------------------------------------------------
-# VPC Network
+# 1. The Three VPCs
 # ------------------------------------------------------------------------------
-resource "google_compute_network" "vpc_network" {
-  name                    = "${var.network_name}-vpc"
+resource "google_compute_network" "mgmt_vpc" {
+  name                    = "${var.project_name}-mgmt-vpc"
   auto_create_subnetworks = false
-  routing_mode            = "REGIONAL"
-  description             = "Main VPC for the network hub"
+}
+
+resource "google_compute_network" "untrust_vpc" {
+  name                    = "${var.project_name}-untrust-vpc"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_network" "trust_vpc" {
+  name                    = "${var.project_name}-trust-vpc"
+  auto_create_subnetworks = false
 }
 
 # ------------------------------------------------------------------------------
-# Subnets
+# 2. The Subnets
 # ------------------------------------------------------------------------------
+resource "google_compute_subnetwork" "mgmt_subnet" {
+  name          = "${var.project_name}-mgmt-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.mgmt_vpc.id
+}
 
-# Public Subnet
-resource "google_compute_subnetwork" "public_subnet" {
-  name          = "${var.network_name}-public-subnet"
+resource "google_compute_subnetwork" "untrust_subnet" {
+  name          = "${var.project_name}-untrust-subnet"
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
-  network       = google_compute_network.vpc_network.id
-  description   = "Public subnet for instances with external IPs"
-  log_config {
-    flow_sampling        = 0.5
-    aggregation_interval = "INTERVAL_1_MIN"
-    metadata             = "INCLUDE_ALL_METADATA"
-  }
+  network       = google_compute_network.untrust_vpc.id
 }
 
-# Private Subnet
-resource "google_compute_subnetwork" "private_subnet" {
-  name                     = "${var.network_name}-private-subnet"
+# GKE Trust Subnet (Moved here from your main.tf to keep networking central)
+resource "google_compute_subnetwork" "trust_subnet" {
+  name                     = "${var.project_name}-trust-subnet"
   ip_cidr_range            = "10.0.2.0/24"
   region                   = var.region
-  network                  = google_compute_network.vpc_network.id
+  network                  = google_compute_network.trust_vpc.id
   private_ip_google_access = true
-  description              = "Private subnet for instances without external IPs"
-  log_config {
-    flow_sampling        = 0.5
-    aggregation_interval = "INTERVAL_1_MIN"
-    metadata             = "INCLUDE_ALL_METADATA"
+
+  secondary_ip_range {
+    range_name    = "gke-pod-range"
+    ip_cidr_range = "10.11.0.0/20"
+  }
+  secondary_ip_range {
+    range_name    = "gke-service-range"
+    ip_cidr_range = "10.12.0.0/20"
   }
 }
 
 # ------------------------------------------------------------------------------
-# Cloud Router & NAT Gateway
+# 3. The Magic Route (Forcing GKE through the Firewall)
 # ------------------------------------------------------------------------------
-
-# A Cloud Router is required for Cloud NAT
-resource "google_compute_router" "router" {
-  name    = "${var.network_name}-router"
-  region  = var.region
-  network = google_compute_network.vpc_network.id
-  bgp {
-    asn = 64514
-  }
+resource "google_compute_route" "default_trust_route" {
+  name             = "${var.project_name}-trust-to-fw"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.trust_vpc.name
+  # This points to the firewall's Trust interface IP (we'll assign this to the FW)
+  next_hop_address = "10.0.2.10" 
+  priority         = 100
 }
 
-# Cloud NAT for private subnet outbound traffic
-resource "google_compute_router_nat" "nat_gateway" {
-  name                               = "${var.network_name}-nat-gateway"
-  router                             = google_compute_router.router.name
-  region                             = var.region
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-  nat_ip_allocate_option             = "AUTO_ONLY"
-
-  subnetwork {
-    name                    = google_compute_subnetwork.private_subnet.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
-
-  log_config {
-    enable = true
-    filter = "ERRORS_ONLY"
-  }
-}
-
-# ------------------------------------------------------------------------------
-# Firewall Rules
-# ------------------------------------------------------------------------------
-
-# Allow internal traffic within the VPC
-resource "google_compute_firewall" "allow_internal" {
-  name    = "${var.network_name}-allow-internal"
-  network = google_compute_network.vpc_network.name
-  allow {
-    protocol = "all"
-  }
-  source_ranges = ["10.0.1.0/24", "10.0.2.0/24"]
-  description   = "Allow all traffic between subnets"
-}
-
-# Allow SSH traffic from anywhere to the public subnet
-resource "google_compute_firewall" "allow_ssh" {
-  name    = "${var.network_name}-allow-ssh"
-  network = google_compute_network.vpc_network.name
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
+# Allow external access to Mgmt and Untrust
+resource "google_compute_firewall" "allow_mgmt" {
+  name          = "${var.project_name}-allow-mgmt"
+  network       = google_compute_network.mgmt_vpc.name
   source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["ssh-allowed"] # Apply this tag to VMs you want to SSH into
-  description   = "Allow SSH from the internet to tagged instances"
-}
-
-# Allow HTTP/HTTPS traffic from anywhere to the public subnet
-resource "google_compute_firewall" "allow_http_https" {
-  name    = "${var.network_name}-allow-http-https"
-  network = google_compute_network.vpc_network.name
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443"]
-  }
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["webserver"] # Apply this tag to your web servers
-  description   = "Allow HTTP/HTTPS from the internet to tagged instances"
+  allow { protocol = "tcp" ports = ["22", "443"] }
 }
